4889ad3a17a607d82ca0038b6f39f3eb23b05834
Karan Oza <50609276+karanoza@users.noreply.github.com>
2025-12-16 17:07:51 +0530

    Added navbar and created restaurants card

--- Food Delievery App/App.js
+++ Food Delievery App/App.js
@@ -21,9 +21,36 @@ const Header = () => {
   )
 }
 
+const RestaurantCard = () => {
+  return (
+    <div className="res-card">
+      <img className="res-logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/McDonald%27s_logo.svg/2560px-McDonald%27s_logo.svg.png" alt="restaurant-logo" />
+      <h3>McDonald's</h3>
+      <h4>Burgers, Beverages, Fast food</h4>
+      <h4>4.6 Stars</h4>
+      <h4>15-20 Mins</h4>
+    </div>
+  )
+}
+
+const Body = () => {
+  return (
+    <div className="body">
+     <div className="search">Search</div>
+      <div className="res-container">
+          <RestaurantCard />
+           <RestaurantCard />
+            <RestaurantCard />
+            
+      </div>
+    </div>
+  )
+}
+
 const AppLayout = () => {
   return <div className="app">
     <Header />
+    <Body />
   </div>
 }
 
