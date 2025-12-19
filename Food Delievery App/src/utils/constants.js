4bfa6861f96a0370c0b2b4a9718a14cadfcf5b26
Karan Oza <50609276+karanoza@users.noreply.github.com>
2025-12-19 13:44:21 +0530

    added constants

---
+++ Food Delievery App/src/utils/constants.js
@@ -0,0 +1,6 @@
+const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+
+const LOGO_URL = "https://upload.wikimedia.org/wikipedia/commons/7/75/Zomato_logo.png";
+
+export { CDN_URL, LOGO_URL };
