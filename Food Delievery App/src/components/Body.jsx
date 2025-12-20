fd821585e955361d3fe2bac53848f6b2bd3ffcef
Karan Oza <50609276+karanoza@users.noreply.github.com>
2025-12-20 10:31:16 +0530

    Added buttons

--- Food Delievery App/src/components/Body.jsx
+++ Food Delievery App/src/components/Body.jsx
@@ -4,7 +4,13 @@ import resList from "../utils/data";
 const Body = () => {
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter-section">
+        <button className="filter-btn">Top Rated</button>
+        <button className="filter-btn">Cost: Low to High</button>
+        <button className="filter-btn">Cost: High to Low</button>
+        <button className="filter-btn">Delivery Time</button>
+        <button className="filter-btn">Cuisines</button>
+      </div>
       <div className="res-container">
 
         {resList.map((restaurant) => (
