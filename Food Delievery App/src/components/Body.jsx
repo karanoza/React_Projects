d49f18b0bb6572e1d27f4454e5ea2271d540c8a6
Karan Oza <50609276+karanoza@users.noreply.github.com>
2026-01-06 17:07:41 +0530

    Adding sort and filter functionality

--- Food Delievery App/src/components/Body.jsx
+++ Food Delievery App/src/components/Body.jsx
@@ -1,19 +1,46 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/data";
+import { useState } from "react";
 
 const Body = () => {
+  //local state variable for restaurants
+  const [restaurants, setRestaurants] = useState(resList);
+ 
+  //Normal JS variable
+  let listOfRestaurants = [];
+
+
   return (
     <div className="body">
       <div className="filter-section">
-        <button className="filter-btn">Top Rated</button>
-        <button className="filter-btn">Cost: Low to High</button>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = restaurants.filter((res) => res.info.avgRating > 4.4);
+            setRestaurants(filteredList);
+            console.log("Filtered List:", filteredList);
+          }}
+        >
+          Top Rated
+        </button>
+        <button className="filter-btn"
+         onClick={() => {
+            const sortedList = [...restaurants].sort((a, b) => {
+              const costA = parseInt(a.info.costForTwo.replace('₹', '').split(' ')[0]);
+              const costB = parseInt(b.info.costForTwo.replace('₹', '').split(' ')[0]);
+              return costA - costB;
+            });
+            setRestaurants(sortedList);
+            console.log("Sorted List:", sortedList);
+           
+          }}
+          >Cost: Low to High</button>
         <button className="filter-btn">Cost: High to Low</button>
         <button className="filter-btn">Delivery Time</button>
         <button className="filter-btn">Cuisines</button>
       </div>
       <div className="res-container">
-
-        {resList.map((restaurant) => (
+        {restaurants.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant.info} />
         ))}
       </div>
