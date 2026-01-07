3713c13e794fb061446836072587196cc04861f1
Karan Oza <50609276+karanoza@users.noreply.github.com>
2026-01-07 15:43:35 +0530

    Added high to low button function

--- Food Delievery App/src/components/Body.jsx
+++ Food Delievery App/src/components/Body.jsx
@@ -5,10 +5,9 @@ import { useState } from "react";
 const Body = () => {
   //local state variable for restaurants
   const [restaurants, setRestaurants] = useState(resList);
- 
+
   //Normal JS variable
   let listOfRestaurants = [];
-
 
   return (
     <div className="body">
@@ -16,26 +15,54 @@ const Body = () => {
         <button
           className="filter-btn"
           onClick={() => {
-            const filteredList = restaurants.filter((res) => res.info.avgRating > 4.4);
+            const filteredList = restaurants.filter(
+              (res) => res.info.avgRating > 4.3
+            );
             setRestaurants(filteredList);
             console.log("Filtered List:", filteredList);
           }}
+          // Filtering restaurants with rating greater than 4.3
         >
           Top Rated
         </button>
-        <button className="filter-btn"
-         onClick={() => {
+        <button
+          className="filter-btn"
+          onClick={() => {
             const sortedList = [...restaurants].sort((a, b) => {
-              const costA = parseInt(a.info.costForTwo.replace('₹', '').split(' ')[0]);
-              const costB = parseInt(b.info.costForTwo.replace('₹', '').split(' ')[0]);
+              const costA = parseInt(
+                a.info.costForTwo.replace("₹", "").split(" ")[0]
+              );
+              const costB = parseInt(
+                b.info.costForTwo.replace("₹", "").split(" ")[0]
+              );
               return costA - costB;
             });
+            // Update the state with the sorted list
             setRestaurants(sortedList);
             console.log("Sorted List:", sortedList);
-           
           }}
-          >Cost: Low to High</button>
-        <button className="filter-btn">Cost: High to Low</button>
+        >
+          Cost: Low to High
+        </button>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const sortedList = [...restaurants].sort((a, b) => {
+              const costA = parseInt(
+                a.info.costForTwo.replace("₹", "").split(" ")[0]
+              );
+              const costB = parseInt(
+                b.info.costForTwo.replace("₹", "").split(" ")[0]
+              );
+              return costB - costA;
+            });
+            // Update the state with the sorted list
+            setRestaurants(sortedList);
+            console.log("Sorted List:", sortedList);
+          }}
+        >
+          Cost: High to Low
+        </button>
         <button className="filter-btn">Delivery Time</button>
         <button className="filter-btn">Cuisines</button>
       </div>
