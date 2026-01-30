bb097a5ce67b346a80179696dc207df08b352a77
Karan Oza <50609276+karanoza@users.noreply.github.com>
2026-01-30 18:21:55 +0530

    update the state hook in both class and fucntion component

--- Food Delievery App/src/components/User.jsx
+++ Food Delievery App/src/components/User.jsx
@@ -1,13 +1,19 @@
 import { useState } from "react";
 
 const User = ({ name }) => {
-  const [count] = useState(0);
-  const [count2] = useState(1);
+  const [count, setCount] = useState(1);
+  const [count2, setCount2] = useState(0);
+
+ 
+ 
   return (
     <div>
       <h2>{name}'s Profile</h2>
       <h3>Count: {count}</h3>
+      <button onClick={() => setCount(count + 2)}>Odd Count</button>
       <h3>Count2: {count2}</h3>
+      <button onClick={() => setCount2(count2 + 2)}>Even Count2</button>
+
       <p>This is the user profile component.</p>
     </div>
   );
--- Food Delievery App/src/components/UserClass.jsx
+++ Food Delievery App/src/components/UserClass.jsx
@@ -1,29 +1,37 @@
 import React from "react";
 
 class UserClass extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("UserClass Constructor", props);
 
-    constructor(props) {
-        super(props);
-        console.log("UserClass Constructor", props);
-
-        this.state = {
-            count: 0,
-            count2: 1
-        };
-    }
-    render() {
-        const { name, location } = this.props;
-        const { count } = this.state;
-        return (
-            <div>
-                <h2>{name} base Profile</h2>
-                <h3>Count: {count}</h3>
-                <h3>Count2: {this.state.count2}</h3>
-                <p>This is the user class base profile component.</p>
-                <p>Location: {location}</p>
-            </div>
-        );
-    }
+    this.state = {
+      count: 0,
+      count2: 1,
+    };
+  }
+  render() {
+    const { name, location } = this.props;
+    const { count } = this.state;
+    return (
+      <div>
+        <h2>{name} base Profile</h2>
+        <h3>Count: {count}</h3>
+        {/* Never Update State variable directly like this.state.count = this.state.count + 1 */}
+        <button onClick={() => this.setState({ count: this.state.count + 2 })}>
+          Even Count
+        </button>
+        <h3>Count2: {this.state.count2}</h3>
+        <button
+          onClick={() => this.setState({ count2: this.state.count2 + 2 })}
+        >
+          Odd Count
+        </button>
+        <p>This is the user class base profile component.</p>
+        <p>Location: {location}</p>
+      </div>
+    );
+  }
 }
 
 export default UserClass;
