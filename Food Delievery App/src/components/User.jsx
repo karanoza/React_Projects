ac8d7045c9f015354d8c65f6c244536c6dd61f7d
Karan Oza <50609276+karanoza@users.noreply.github.com>
2026-01-30 17:59:49 +0530

    state hook difference between class and functional component

--- Food Delievery App/src/components/User.jsx
+++ Food Delievery App/src/components/User.jsx
@@ -1,7 +1,13 @@
+import { useState } from "react";
+
 const User = ({ name }) => {
+  const [count] = useState(0);
+  const [count2] = useState(1);
   return (
     <div>
       <h2>{name}'s Profile</h2>
+      <h3>Count: {count}</h3>
+      <h3>Count2: {count2}</h3>
       <p>This is the user profile component.</p>
     </div>
   );
--- Food Delievery App/src/components/UserClass.jsx
+++ Food Delievery App/src/components/UserClass.jsx
@@ -5,12 +5,20 @@ class UserClass extends React.Component {
     constructor(props) {
         super(props);
         console.log("UserClass Constructor", props);
+
+        this.state = {
+            count: 0,
+            count2: 1
+        };
     }
     render() {
         const { name, location } = this.props;
+        const { count } = this.state;
         return (
             <div>
                 <h2>{name} base Profile</h2>
+                <h3>Count: {count}</h3>
+                <h3>Count2: {this.state.count2}</h3>
                 <p>This is the user class base profile component.</p>
                 <p>Location: {location}</p>
             </div>
