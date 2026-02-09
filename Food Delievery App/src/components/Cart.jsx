238412955bd255d74990d72ee04daa677eb8c324
Karan Oza <50609276+karanoza@users.noreply.github.com>
2026-02-09 17:35:28 +0530

    componentDidMount understanding

--- Food Delievery App/src/components/Cart.jsx
+++ Food Delievery App/src/components/Cart.jsx
@@ -1,13 +1,29 @@
 import  User from "./User";
 import UserClass from "./UserClass";
-const Cart = () => {
-  return (
-    <div>
-      <h1>Class and functional Component differences</h1>
-      <User name={"Karan Oza"} />
-  <UserClass name={"Karan Oza ka class"} location={"Pune"} />
-    </div>
-  );
-};
+import { Component } from "react";
+
+
+class Cart extends Component {
+  constructor(props) {
+    super(props);
+    console.log("Parent Cart Constructor", props);
+
+  
+  }
+
+  componentDidMount() {
+    console.log("Parent Cart componentDidMount");
+  }
+  render() {
+    return (
+      console.log("Parent Cart Render"),
+      <div>
+        <h1>Class and functional Component differences</h1>
+        <User name={"Karan Oza"} />
+        <UserClass name={"Karan Oza ka class"} location={"Pune"} />
+      </div>
+    );
+  }
+}
 
 export default Cart;
--- Food Delievery App/src/components/UserClass.jsx
+++ Food Delievery App/src/components/UserClass.jsx
@@ -3,17 +3,22 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
-    console.log("UserClass Constructor", props);
+    console.log("Child UserClass Constructor", props);
 
     this.state = {
       count: 0,
       count2: 1,
     };
   }
+
+  componentDidMount() {
+    console.log("Child UserClass componentDidMount");
+  }
   render() {
     const { name, location } = this.props;
     const { count } = this.state;
     return (
+      console.log("Child UserClass Render", this.props),
       <div>
         <h2>{name} base Profile</h2>
         <h3>Count: {count}</h3>
