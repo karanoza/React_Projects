a1afb83b7dc1cd18f138c2b70ace49fb3e70900e
Karan Oza <50609276+karanoza@users.noreply.github.com>
2026-02-19 16:28:22 +0530

    added api in componentDidMount

--- Food Delievery App/src/components/User.jsx
+++ Food Delievery App/src/components/User.jsx
@@ -1,20 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const User = ({ name }) => {
   const [count, setCount] = useState(1);
   const [count2, setCount2] = useState(0);
 
- 
+ useEffect(() => {
+    console.log("useEffect called for count:", count);
+  }, []);
+
+  async function getUserInfo() {
+    // Simulate an API call to fetch user information.
+  }
  
   return (
     <div>
-      <h2>{name}'s Profile</h2>
-      <h3>Count: {count}</h3>
-      <button onClick={() => setCount(count + 2)}>Odd Count</button>
-      <h3>Count2: {count2}</h3>
-      <button onClick={() => setCount2(count2 + 2)}>Even Count2</button>
-
-      <p>This is the user profile component.</p>
+      
     </div>
   );
 };
--- Food Delievery App/src/components/UserClass.jsx
+++ Food Delievery App/src/components/UserClass.jsx
@@ -6,38 +6,42 @@ class UserClass extends React.Component {
     console.log(this.props.name + "Child UserClass Constructor", props);
 
     this.state = {
-      count: 0,
-      count2: 1,
+      userInfo: {
+        name: "Dummy",
+        Twitter: "Yo Developer",
+        location: "Earth",
+        avatar_url:"https://www.dummyimage.com/avatar.png",
+        company: "Dummy Company",
+      },
     };
   }
 
 // componentDidMount is a lifecycle method that is called after the component is rendered for the first time. It is used to perform side effects such as fetching data via API calls, setting up subscriptions, or manually changing the DOM in React components.
 
-  componentDidMount() {
+  async componentDidMount() {
     console.log(this.props.name +"Child UserClass componentDidMount");
     //first constructor is called then render is called and then componentDidMount is called. componentDidMount is called only once in the lifecycle of a component.
     //Thats why we do API calls in componentDidMount because we want to fetch data after the component is rendered for the first time. If we do API calls in constructor then it will be called every time the component is rendered which is not good for performance.
+
+    const data = await fetch("https://api.github.com/users/karanoza");
+    const json = await data.json();
+    console.log("GitHub Data:", json);
+
+    this.setState({
+      userInfo: json,
+    });
   }
   render() {
-    const { name, location } = this.props;
-    const { count } = this.state;
+    
     return (
-      console.log(this.props.name + "Child UserClass Render"),
-      <div>
-        <h2>{name} base Profile</h2>
-        <h3>Count: {count}</h3>
-        {/* Never Update State variable directly like this.state.count = this.state.count + 1 */}
-        <button onClick={() => this.setState({ count: this.state.count + 2 })}>
-          Even Count
-        </button>
-        <h3>Count2: {this.state.count2}</h3>
-        <button
-          onClick={() => this.setState({ count2: this.state.count2 + 2 })}
-        >
-          Odd Count
-        </button>
-        <p>This is the user class base profile component.</p>
-        <p>Location: {location}</p>
+      <div className="user-card">
+        <img src={this.state.userInfo.avatar_url} alt="Avatar" width="100" height="100" />
+        <div className="user-card-details">
+          <h2>Name: {this.state.userInfo.name}</h2>
+          <h3>Twitter: {this.state.userInfo.twitter_username}</h3>
+          <h4>Location: {this.state.userInfo.location}</h4>
+          <h4>Company: {this.state.userInfo.company}</h4>
+        </div>
       </div>
     );
   }
